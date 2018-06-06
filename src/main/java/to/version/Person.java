b4839b315b853602a4e0c53a648e2ede7114eeed
michaeljohnson <mjohns39@mix.wvu.edu>
2018-06-06 19:34:37 -0400

    simple version

--- src/main/java/to/version/Person.java
+++ src/main/java/to/version/Person.java
@@ -1,61 +1,43 @@
 package to.version;
 
+import static java.util.stream.Collectors.toSet;
+
 import java.util.Collection;
 import java.util.Set;
 
-import static java.util.Collections.emptyList;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class Person {
 
     private long id;
     private String name;
     private Set<Person> friends;
 
-    public Person(long id) {
-        this.id = id;
-    }
+//    public Person(long id) {
+//        this.id = id;
+//    }
+//
+//    public Person(String name, Set<Person> friends) {
+//        this.name = name;
+//        this.friends = friends;
+//    }
 
-    public Person(String name, Set<Person> friends) {
-        this.name = name;
-        this.friends = friends;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Person> getFriends() {
-        return friends;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setFriends(Set<Person> friends) {
-        this.friends = friends;
-    }
 
     /**
      * Where friends who have the exact name
      */
     public Collection<Person> findFriendsByName(String name) {
-        return emptyList();
+        return friends.stream().filter(f -> f.getName().equals(name)).collect(toSet());
     }
 
     /**
      * Where friends who have a name starting with the name provided
      */
     public Collection<Person> findFriendsLikeName(String name) {
-        return emptyList();
+        return friends.stream().filter(f -> f.getName().startsWith(name)).collect(toSet());
     }
 
 }
--- src/test/java/to/version/PersonTest.java
+++ src/test/java/to/version/PersonTest.java
@@ -1,27 +1,62 @@
 package to.version;
 
+import static java.util.stream.Collectors.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class PersonTest {
 
-    private Person person;
+    private Person dee;
+    private Person michael;
+    private Person richard;
+    
+    private Map<String, Person> phoneBook = new HashMap<>();
 
     @Before
     public void setUp() throws Exception {
-
+    	
+    	richard = Person.builder().id(1).name("Richard").friends(Collections.unmodifiableSet(Collections.emptySet())).build();
+    	
+    	dee = Person.builder().id(2).name("Dee").friends(Stream.of(richard).collect(toSet())).build();
+    	
+    	michael = Person.builder().id(3).name("Michael").friends(Stream.of(dee, richard).collect(toSet())).build();
+    	
+    	phoneBook.put(richard.getName(), richard);
+    	phoneBook.put(dee.getName(), dee);
+    	phoneBook.put(michael.getName(), michael);
     }
 
     @Test
     public void verify_find_by_name() {
-
+    	assertNotNull(dee.findFriendsByName("Richard"));
     }
 
     @Test
     public void verify_find_like_name() {
-
+    	assertNotNull(dee.findFriendsLikeName("Ric"));
     }
 
+    @Test
+    public void find_all_people_with_certain_friend() {
+    	Set<Person> result = phoneBook.entrySet().stream()
+    			.filter(e -> !e.getValue().findFriendsByName("Dee").isEmpty())
+    			.flatMap(e -> Stream.of(e.getValue()))
+    			.collect(toSet())
+//    			.collect(toMap(Map.Entry::getKey, Map.Entry::getValue))
+//    			.values().stream()
+//    			.forEach(p -> System.out.println(p.toString()))
+    			;
+    	
+    	assertTrue(result.size() == 1);
+    }
+    
 }
